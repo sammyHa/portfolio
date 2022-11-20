08b00d9501fecc037738d386948e958c745996fc
samimhakimi <samimgooogl@gmail.com>
2022-11-20 15:03:05 -0500

    Updates

---
+++ precache-manifest.20a1a3cf32ac0f6d56d8bbf644f5cd6c.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "a36e5bffb25fe7432df7",
+    "url": "/portfolio/static/css/main.dfbdde47.chunk.css"
+  },
+  {
+    "revision": "a36e5bffb25fe7432df7",
+    "url": "/portfolio/static/js/main.a36e5bff.chunk.js"
+  },
+  {
+    "revision": "ddad22f04f19ad3f4e4e",
+    "url": "/portfolio/static/js/1.ddad22f0.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "eebd360bce5c9f45d02aa98b20c25708",
+    "url": "/portfolio/static/media/img.eebd360b.png"
+  },
+  {
+    "revision": "7dc895980b3d7c2439a7679bac0d5818",
+    "url": "/portfolio/static/media/img.7dc89598.jpeg"
+  },
+  {
+    "revision": "89343b24902db8360812376ac399e2b3",
+    "url": "/portfolio/static/media/img.89343b24.gif"
+  },
+  {
+    "revision": "06d829d389b4b0b07e00baeb4b007f53",
+    "url": "/portfolio/static/media/shot_1.06d829d3.gif"
+  },
+  {
+    "revision": "f2e5319d89508a93dac2b08de640545c",
+    "url": "/portfolio/static/media/shot_2.f2e5319d.gif"
+  },
+  {
+    "revision": "430c1dd8265eed558dc4784c1a7b76dd",
+    "url": "/portfolio/static/media/shot_3.430c1dd8.gif"
+  },
+  {
+    "revision": "5e8a21638b521785839b072cb13bcbe4",
+    "url": "/portfolio/static/media/img.5e8a2163.webp"
+  },
+  {
+    "revision": "e1568b685b850da337ee979d04e6cbc8",
+    "url": "/portfolio/static/media/img2.e1568b68.png"
+  },
+  {
+    "revision": "0e5f352f46843ac5450447d6959b8256",
+    "url": "/portfolio/static/media/img3.0e5f352f.jpeg"
+  },
+  {
+    "revision": "b42f16e9c46c4d71d12261c113a9e40f",
+    "url": "/portfolio/index.html"
+  }
+];
